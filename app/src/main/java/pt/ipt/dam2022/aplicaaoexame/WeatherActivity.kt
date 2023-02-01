37709f19014004f6c9f8b2d006ba805e80e2e517
Pedro Martinho <aluno23299@ipt.pt>
2023-02-01 05:48:29 +0000

    minor changes WeatherActivity.kt

--- app/src/main/java/pt/ipt/dam2022/aplicaaoexame/WeatherActivity.kt
+++ app/src/main/java/pt/ipt/dam2022/aplicaaoexame/WeatherActivity.kt
@@ -2,6 +2,7 @@
 
 package pt.ipt.dam2022.aplicaaoexame
 
+import android.annotation.SuppressLint
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -17,9 +18,9 @@ import java.util.*
 
 class WeatherActivity : AppCompatActivity() {
 
-    val LAT : String = "39.4647"
-    val LON : String = "-8.469"
-    val API : String = "f4e820447d3f6dc3e5581f68d547ef74"
+    val lat : String = "39.4647"
+    val lon : String = "-8.469"
+    val api : String = "f4e820447d3f6dc3e5581f68d547ef74"
 
 
 
@@ -27,11 +28,12 @@ class WeatherActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
 
-        weatherTask().execute()
+        WeatherTask().execute()
     }
-    inner class weatherTask() : AsyncTask<String, Void, String>()
+    @SuppressLint("StaticFieldLeak")
+    inner class WeatherTask() : AsyncTask<String, Void, String>() {
 
-    {
+        @Deprecated("Deprecated in Java")
         override fun onPreExecute() {
             super.onPreExecute()
 
@@ -40,52 +42,50 @@ class WeatherActivity : AppCompatActivity() {
             findViewById<TextView>(R.id.erro).visibility = View.GONE
         }
 
+        @Deprecated("Deprecated in Java")
         override fun doInBackground(vararg p0: String?): String? {
-            var response:String?
-            try {
-                response = URL("https://api.openweathermap.org/data/2.5/weather?lang=pt&units=metric&lat=$LAT&lon=$LON&appid=$API").readText(Charsets.UTF_8)
-            }
-            catch (e: Exception)
-            {
-                response = null
+            val response:String? = try {
+                URL("https://api.openweathermap.org/data/2.5/weather?lang=pt&units=metric&lat=$lat&lon=$lon&appid=$api").readText(Charsets.UTF_8)
+            } catch (e: Exception) {
+                null
             }
             return response
         }
 
+        @Deprecated("Deprecated in Java")
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
             try{
-                val jsonObj = JSONObject(result)
+                val jsonObj = JSONObject(result.toString())
                 val main = jsonObj.getJSONObject("main")
                 val sys = jsonObj.getJSONObject("sys")
                 val wind = jsonObj.getJSONObject("wind")
                 val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
 
                 val update:Long = jsonObj.getLong("dt")
-                val updatet = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(update*1000))
-                val temp = main.getString("temp")+"°C"
+                val updateTempo = "Updated at: "+ SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(Date(update*1000))
+                val temperatura = main.getString("temp")+"°C"
                 val tempMin = "Min Temp: " + main.getString("temp_min")+"°C"
                 val tempMax = "Max Temp: " + main.getString("temp_max")+"°C"
                 val pressao = main.getString("pressure")
                 val humidade= main.getString("humidity")
-                val nsol:Long = sys.getLong("sunrise")
-                val psol:Long = sys.getLong("sunset")
-                val vvento = wind.getString("speed")
-                val descricaotempo = weather.getString("description")
+                val nascerSol:Long = sys.getLong("sunrise")
+                val porSol:Long = sys.getLong("sunset")
+                val ventoSpeed = wind.getString("speed")
+                val descTempo = weather.getString("description")
                 val groupTempo = weather.getString("main")
 
                 val local = jsonObj.getString("name")+", "+sys.getString("country")
 
 
                 findViewById<TextView>(R.id.Localizacao).text = local
-                findViewById<TextView>(R.id.update1).text =  updatet
-                findViewById<TextView>(R.id.Tempo).text = descricaotempo.capitalize(Locale.ROOT)
-                findViewById<TextView>(R.id.Temperatura).text = temp
+                findViewById<TextView>(R.id.update1).text =  updateTempo
+                findViewById<TextView>(R.id.Tempo).text = descTempo.capitalize(Locale.ROOT)
+                findViewById<TextView>(R.id.Temperatura).text = temperatura
                 findViewById<TextView>(R.id.temp_min).text = tempMin
                 findViewById<TextView>(R.id.temp_max).text = tempMax
 
-                var icon = findViewById<ImageView>(R.id.weather_icon)
-                setWeatherConditionIcon(icon,groupTempo)
+                setWeatherConditionIcon(findViewById<ImageView>(R.id.weather_icon),groupTempo)
 
                 findViewById<ProgressBar>(R.id.progresso).visibility = View.GONE
                 findViewById<RelativeLayout>(R.id.Container1).visibility = View.VISIBLE
